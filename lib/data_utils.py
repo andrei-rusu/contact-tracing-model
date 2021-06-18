Fix issues with dynamic edge updates. Now edge weight normalization is the same even if daily or weekly aggregation is performed. The script running the SocialEvolution experiment is now defaulted to running daily aggregations. Add separate infseed variable for first infected, which can also be selected based on the dynamic netseed if infseed is not supplied.

lib/data_utils.py
@@ -12,6 +12,7 @@ def load_socialevol(agg='D', proxim_prob=None):
     dest = 'dst'
     date = 'time'
     ct = 'counts'
+    week_idx = 'weekid'
     time_idx = 'timeid'
     sms.rename(columns={'user.id': uid, 'dest.user.id.if.known': dest}, inplace=True)
     call.rename(columns={'user_id': uid, 'dest_user_id_if_known': dest, 'time_stamp': date}, inplace=True)
@@ -23,17 +24,17 @@ def load_socialevol(agg='D', proxim_prob=None):
     # we select only the proximity entries that have a higher chance of having been on the same floor
     proxim_filt = proxim[proxim.prob2 >= proxim_prob] if proxim_prob is not None else proxim
     
-    cols = [time_idx, date, uid, dest]
+    cols = [time_idx, date, week_idx, uid, dest]
     
     def get_counts_by_time_agg(df):
         df = df.astype({uid: 'int', dest: 'int', date: 'datetime64[D]'}, copy=False)
         year = df[date].dt.isocalendar().year
         # we only consider dates which span weeks from 2008 and 2009
         df = df[year.isin((2008, 2009))]
-        if agg == 'W':
-            df[time_idx] = (year - 2008) * 52 + df[date].dt.isocalendar().week - 1
-        else:
-            df[time_idx] = (year - 2008) * 52 + df[date].dt.dayofyear - 1
+        # we add by default a column specifying the week id (start index 0) of the datapoints for filtering purposes
+        df[week_idx] = (year - 2008) * 52 + (df[date].dt.isocalendar().week - 1)
+        # based on the value chosen for agg, we'll have either the week id or the day id as a time id (start index 0)
+        df[time_idx] = df[week_idx] if agg == 'W' else (year - 2008) * 366 + (df[date].dt.dayofyear - 1)
         # eliminate different orders for the 2 nids
         df[[uid, dest]] = np.sort(df[[uid, dest]], axis=1)
         # group by timeid, and node ids
@@ -59,21 +60,32 @@ class DataLoader():
         # load data as specified by the dataset and agg args
         self.data = globals()['load_' + dataset](agg, proxim_prob)
         
-    def get_edge_data_for_time(self, which_inf=0, which_tr=1, date_fr='2009-01-05', date_to='2009-06-14'):
+    def get_edge_data_for_time(self, which_inf=0, which_tr=1, time_fr='2009-01-05', time_to='2009-06-14', use_week_index=True):
+        """
+        date_fr/to can either be strings marking dates or integers marking the time ids (day or week ids) directly
+        """
         pd.options.mode.chained_assignment = None
         if which_inf is None or which_inf < 0:
             raise ValueError('A valid network must be selected for the infection net')
         vars_to_query = ['uid', 'dst', 'counts']
         inf = self.data[which_inf]
-        # select data inbetween these dates
-        inf = inf.loc[inf.time.between(date_fr, date_to)]
-        # correct first week id based on infection network min week
+        # filtering data can be done based on actual datetime (if time_fr is in str format), by weekid (if use_week_index=True), or by timeid otherwise
+        if isinstance(time_fr, str):
+            time_selector = 'time'
+        elif use_week_index:
+            time_selector = 'weekid'
+        else:
+            time_selector = 'timeid'
+        # select data inbetween these dates/time_ids based on time_selector chosen
+        inf = inf.loc[inf[time_selector].between(time_fr, time_to)]
+        # make first time id point 0, and amend the other entries to reflect this reindexing
         offset = inf.timeid.min()
         inf.timeid -= offset
         # get the time indexes
         time_keys = set(inf.timeid)
         nodes = set(inf.uid).union(inf.dst)
-        norm_for_avg = (max(time_keys) - min(time_keys) + 1) * len(nodes)
+        # we do not dilute the edge weights if there are holes in the time_keys -> assume data collection problem rather than no interaction
+        norm_for_avg = (inf.time.max() - inf.time.min()).days * len(nodes)
         if which_tr is not None and which_tr >= 0:
             tr = self.data[which_tr]
             # filter tracing net data to include only nodes available in the infection net and dates inbetween what is specified
@@ -92,7 +104,8 @@ class DataLoader():
         else:
             edges = {str(int(timeid)): (inf.loc[inf.timeid == timeid][vars_to_query].values, None) for timeid in time_keys}
         # normalization <W> for infection net - average over timesteps of averages over nodes
-        edges['Wi'] = inf.counts.sum() / (len(time_keys) * len(nodes))
+        sum_all = inf.counts.sum()
+        edges['Wi'] = 2 * sum_all / norm_for_avg
         # we also set a key for retrieving node ids in this data
         edges['nid'] = nodes
         return edges

lib/gridsearch.py
@@ -4,17 +4,17 @@ from sklearn.model_selection import ParameterGrid
 
 
 pa_vals = [.2]
-tau_vals = np.array([.1, .5, 1., 1.5, 2.])
+tau_vals = np.array([.1, .2, .5, 1., 1.5, 2.])
 GRID = [
     # When taut = 0, it does not matter whether its dual/triad, or the degree of overlap/uptake (so make them 1)
-    {'uptake': [1],
-     'taut': [0],
-     'taur': tau_vals,
-     'pa': pa_vals,
-     'overlap': [1],
-     'dual': [1],
-    },
-    # When taut != 0 and dual=1 scenario, try all overlaps OR uptakes
+#     {'uptake': [1],
+#      'taut': [0],
+#      'taur': tau_vals,
+#      'pa': pa_vals,
+#      'overlap': [1],
+#      'dual': [1],
+#     },
+    # When taut != 0 and dual=1 scenario, try all overlaps OR uptakes (used for digital tracing only)
     {'uptake': np.linspace(.1, 1., 7),
      'taut': [10],
      'taur': tau_vals,

lib/network.py
@@ -266,7 +266,7 @@ class Network(nx.Graph):
         Tf ids is not supplied, this method does guarantees n additions will be performed past the max id or current counter
         """
         if ids is None:
-            # first element to be added is either max(nid) + 1 OR current + 1 = next - 1 + 1
+            # first element to be added is either max(nid) + 1 OR current_count + 1 = next_count - 1 + 1
             nxt = max(self.nodes, default=next(self.cont)-1) + 1
             ids = range(nxt, nxt + n)
             # increment count to reflect n additions
@@ -449,8 +449,11 @@ class Network(nx.Graph):
             self.update_counts_with_traced() if update_with_traced else self.update_counts()
         
             
-    def avg_degree(self):
-        return np.mean(list(dict(self.degree()).values()))
+    def avg_degree(self, use_weights=False):
+        if use_weights:
+            return np.mean(list(dict(self.degree(weight='weight')).values()))
+        else:
+            return 2 * len(self.edges) / len(self.nodes)
     
     def compute_efforts(self, taur):
         node_states = self.node_states
@@ -471,7 +474,7 @@ class Network(nx.Graph):
         return round(taur * randEffortAcum, 2), round(self.count_importance * tracingEffortAccum, 2)
     
         
-    def draw(self, pos=None, show=True, ax=None, layout_type='spring_layout', seed=43, legend=True, full_name=False, model='covid'):
+    def draw(self, pos=None, show=True, ax=None, layout_type='spring_layout', seed=43, legend=True, full_name=False, model='covid', degree_size=True):
         # get the node state list
         node_states = self.node_states
         # for the true network, colors for all nodes are based on their state
@@ -502,9 +505,14 @@ class Network(nx.Graph):
         else:
             # if both were None, generate a new layout with the seed and use it for drawing
             pos = self.generate_layout(layout_type=layout_type, seed=seed)
+            
+        node_size = None    
+        if degree_size:
+            d = nx.degree(self)
+            node_size = [(d[node]+1) * 100 for node in self.nodes]
                         
         # draw graph
-        nx.draw(self, pos=pos, node_color=colors, ax=ax, with_labels=True)
+        nx.draw(self, pos=pos, node_color=colors, ax=ax, with_labels=True, node_size=node_size)
         
         if legend:
             model_states = MODEL_TO_STATES[model]
@@ -591,8 +599,8 @@ def get_from_predef(nx_or_edgelist, rem_orphans=False, count_importance=1, inet=
         ids = nx_or_edgelist.nodes
         edges = list(nx_or_edgelist.edges(data=True))
     except AttributeError:
-        # allow for user inputted nids to be used if the 'nid' key is set
-        ids = kwargs['nettype'].get('nid', None)
+        # allow for user inputted nids to be used if the 'nid' key is set in the optional arg 'nettype'
+        ids = kwargs.get('nettype', {}).get('nid', None)
         # otherwise, infer the nodes from the edge list
         if not ids:
             # Need to obtain the set of node ids from the edge list

lib/stats.py
@@ -44,6 +44,7 @@ class StatsProcessor():
         # local for efficiency
         args = self.args
         efforts = args.efforts
+        first_inf = args.first_inf
         
         summary = defaultdict(dict)
         summary['args'] = vars(args).copy()
@@ -83,16 +84,20 @@ class StatsProcessor():
             h_max = []
             h_timeofmax = []
             
-            # time range right limits
+            # time range right-limits
             time_range_limits = np.zeros(splits)
             # set upper time limits for each accumulator[:][j] pile
-            for j in range(splits):
-                time_range_limits[j] = max_time * (j + 1) / splits
+            # accumulator[:][0] is reserved for the initial configuration of the simulation, so only (splits - 1) slots remain available
+            available_time_splits = splits - 1
+            for j in range(1, splits):
+                time_range_limits[j] = max_time * j / available_time_splits
 
             # number of different vars computed across time
             num_vars = 13
             # holds simulation result parameters over time
             accumulator = np.zeros((num_vars, splits, len_series))
+            # set the first infected number for time 0 (second-dim-id = 0); indices in first dim: active-inf, total-inf, total-infectious
+            accumulator[np.ix_([0, 1, 9], [0], range(len_series))] = first_inf
             # indexes of early stopped
             idx_early_stopped = []
             # array of arrays containing multiple Reff/growth_rates measurements sampled every r_window
@@ -106,7 +111,7 @@ class StatsProcessor():
                 # counter var which will be used to index the current result in the series at a specific time slot
                 serI = 1
                 # iterate over "time splits"
-                for j in range(splits):
+                for j in range(1, splits):
                     # increment until the upper time limit of the j-th split - i.e. time_range_limits[j]
                     # is exceeded by an event (and therefore the event will be part of block j + 1)
                     while serI < len(ser) and time_range_limits[j] > ser[serI].time:
@@ -130,17 +135,14 @@ class StatsProcessor():
                     accumulator[12][j][ser_index] = last_idx.totalNonCompliant
 
                 
-                # get first event -> needed to compute peaks and growth rates
-                first_event = ser[0]
+                # Get Peak of Infection, Peak of Hospitalization, Time of peaks, and growth rate for r_window
+                # 'reference_' vars are for growths, '_max' vars are for peaks
+                this_i_max = reference_past_active_inf = reference_past_total_inf = first_inf
+                this_h_max = 0
+                this_i_timeofmax = this_h_timeofmax = reference_past_time = 0
                 growth = []
                 active_growth = []
-
-                # Get Peak of Infection, Peak of Hospitalization, Time of peaks, and Reff for r_window
-                # reference_ vars are for growths, _max vars are for peaks
-                this_i_max = reference_past_active_inf = first_event.nI + first_event.nE
-                this_h_max = first_event.nH
-                reference_past_total_inf = first_event.totalInfected
-                this_i_timeofmax = this_h_timeofmax = reference_past_time = first_event.time
+                
                 for event in ser:
                     event_time = event.time
                     active_infected = event.nI + event.nE
@@ -185,7 +187,7 @@ class StatsProcessor():
                 current_stats = accumulator[i]
                 stats_for_timed_parameters.append(get_statistics(current_stats, compute='mean', axis=1, avg_without_idx=without_idx))
                 stats_for_laststamp.append(get_statistics(current_stats[-1], compute='all', avg_without_idx=without_idx)[0])
-                
+                                
             # compute averages and other statistics for the peak simulation results
             stats_for_max_inf = get_statistics(i_max, compute='all', avg_without_idx=without_idx)[0]
             stats_for_timeofmax_inf = get_statistics(i_timeofmax, compute='mean+wo', avg_without_idx=without_idx)[0]
@@ -257,7 +259,7 @@ class StatsProcessor():
             current['active-growth'] = stats_for_active_growth
 
         if printit:
-            # if printit is 1 and nettype is a predefined supplied network (i.e. not a str), the value in the summary dict will be overwritten
+            # if printit is 1 and nettype is a predefined supplied network (i.e. not a str), the repr of the net in the summary will be overwritten to reduce space
             if printit == 1 and not isinstance(summary['args']['nettype'], str):
                 summary['args']['nettype'] = 'predefined'
             print(json.dumps(summary, cls=ut.NumpyEncoder))